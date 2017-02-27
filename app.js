d4794032c1de2d9e7e24ce2b6ac4dfb12cf59635
Nathan Edlen <nathan.edlen@gmail.com>
2017-02-27 14:17:23 -0700

    Initial

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(port)
